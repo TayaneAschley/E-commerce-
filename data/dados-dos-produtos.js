41d961e5fdd3b4c1574ecf8dc62bb55b2adf436b
Tayane <asch.tayane@gmail.com>
2022-08-12 12:59:17 -0300

    feat: dados dos produtos

---
+++ data/dados-dos-produtos.js
@@ -0,0 +1,116 @@
+export const produtosStarWars = [
+    {
+      id: 1,
+      nome: "Produto XYZ",
+      img: "../assets/img/img-starwars/1.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 2,
+      nome: "Produto XYZ",
+      img: "../assets/img/img-starwars/2.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 3,
+      nome: "Produto XYZ",
+      img: "../assets/img/img-starwars/3.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 4,
+      nome: "Produto XYZ",
+      img: "../assets/img/img-starwars/4.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 5,
+      nome: "Produto XYZ",
+      img: "../assets/img/img-starwars/5.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 6,
+      nome: "Produto XYZ",
+      img: "../assets/img/img-starwars/6.png",
+      valor: "R$60,00",
+    },
+  ];
+  
+  export const produtosConsole = [
+    {
+      id: 1,
+      nome: "Contole XYZ",
+      img: "../assets/img/img-consoles/1.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 2,
+      nome: "Contole e Console XYZ",
+      img: "../assets/img/img-consoles/2.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 3,
+      nome: "Console XYZ",
+      img: "../assets/img/img-consoles/3.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 4,
+      nome: "Controle XYZ",
+      img: "../assets/img/img-consoles/4.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 5,
+      nome: "Console XYZ",
+      img: "../assets/img/img-consoles/5.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 6,
+      nome: "Game boy Color",
+      img: "../assets/img/img-consoles/6.png",
+      valor: "R$60,00",
+    },
+  ];
+  
+  export const produtosDiversos = [
+    {
+      id: 1,
+      nome: "Camisa Atari",
+      img: "../assets/img/img-diversos/1.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 2,
+      nome: "Camisa SNESS",
+      img: "../assets/img/img-diversos/2.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 3,
+      nome: "Controle e Console XYZ",
+      img: "../assets/img/img-diversos/3.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 4,
+      nome: "Game boy Color",
+      img: "../assets/img/img-diversos/4.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 5,
+      nome: "Oculos realidade Virtual",
+      img: "../assets/img/img-diversos/5.png",
+      valor: "R$60,00",
+    },
+    {
+      id: 6,
+      nome: "Pikachu Gigante",
+      img: "../assets/img/img-diversos/6.png",
+      valor: "R$60,00",
+    },
+  ];
