7ff6ec59aae8ca3e097000570658e97e80678990
Tayane <asch.tayane@gmail.com>
2022-08-15 13:32:38 -0300

    feat:  login fica salvo no localStorage

--- javascripts/formulario/login.js
+++ javascripts/formulario/login.js
@@ -1,27 +1,23 @@
-const formulario = document.querySelector("[data-form-login]");
-formulario.addEventListener("submit", (evento) => {
-  evento.preventDefault();
 
-  const email = evento.target.querySelector("[data-email-login]").value;
-  const senha = evento.target.querySelector("[data-senha-login]").value;
+const formulario = document.querySelector("[data-form-login]")
 
-  const dadosUsuarios = `http://localhost:3000/USUARIOS/`;
-  fetch(dadosUsuarios)
-    .then((resposta) => {
-      return resposta.json();
-    })
-    .then((data) => {
-      let validacao = data.find(
-        (e) => e.email == email && e.senha == senha
-      );
-      console.log(validacao);
-      if (validacao) {
-        alert("usuario encontrado");
-        email.value = "";
-        senha.value = "";
-        email.focus();
-      } else {
-        alert("usuario não encontrado");
-      }
-    });
-});
+formulario.addEventListener("submit", (event)=>{
+  event.preventDefault()
+
+  const email = event.target.querySelector("[data-email-login]").value;
+  const senha = event.target.querySelector("[data-senha-login]").value;
+   
+  const usuario = JSON.parse(localStorage.getItem("usuario")) 
+  
+  if(email === usuario.email && senha === usuario.senha){
+    alert("usuario cadastrado");
+  }
+})
+
+
+ 
+
+
+
+
+
