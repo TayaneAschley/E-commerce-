64cf808b2aabb8b188f89f1d66f26eb2e5dbf80b
Tayane <asch.tayane@gmail.com>
2022-08-15 13:37:06 -0300

    feat: adiciona imagens novas nos produtos

--- data/dados-dos-produtos.js
+++ data/dados-dos-produtos.js
@@ -1,116 +1,116 @@
 export const produtosStarWars = [
-    {
-      id: 1,
-      nome: "Produto XYZ",
-      img: "../assets/img/img-starwars/1.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 2,
-      nome: "Produto XYZ",
-      img: "../assets/img/img-starwars/2.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 3,
-      nome: "Produto XYZ",
-      img: "../assets/img/img-starwars/3.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 4,
-      nome: "Produto XYZ",
-      img: "../assets/img/img-starwars/4.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 5,
-      nome: "Produto XYZ",
-      img: "../assets/img/img-starwars/5.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 6,
-      nome: "Produto XYZ",
-      img: "../assets/img/img-starwars/6.png",
-      valor: "R$60,00",
-    },
-  ];
-  
-  export const produtosConsole = [
-    {
-      id: 1,
-      nome: "Contole XYZ",
-      img: "../assets/img/img-consoles/1.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 2,
-      nome: "Contole e Console XYZ",
-      img: "../assets/img/img-consoles/2.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 3,
-      nome: "Console XYZ",
-      img: "../assets/img/img-consoles/3.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 4,
-      nome: "Controle XYZ",
-      img: "../assets/img/img-consoles/4.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 5,
-      nome: "Console XYZ",
-      img: "../assets/img/img-consoles/5.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 6,
-      nome: "Game boy Color",
-      img: "../assets/img/img-consoles/6.png",
-      valor: "R$60,00",
-    },
-  ];
-  
-  export const produtosDiversos = [
-    {
-      id: 1,
-      nome: "Camisa Atari",
-      img: "../assets/img/img-diversos/1.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 2,
-      nome: "Camisa SNESS",
-      img: "../assets/img/img-diversos/2.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 3,
-      nome: "Controle e Console XYZ",
-      img: "../assets/img/img-diversos/3.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 4,
-      nome: "Game boy Color",
-      img: "../assets/img/img-diversos/4.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 5,
-      nome: "Oculos realidade Virtual",
-      img: "../assets/img/img-diversos/5.png",
-      valor: "R$60,00",
-    },
-    {
-      id: 6,
-      nome: "Pikachu Gigante",
-      img: "../assets/img/img-diversos/6.png",
-      valor: "R$60,00",
-    },
-  ];
+  {
+    id: 1,
+    nome: "caneca StarWars",
+    img: "https://www.bftronic.com.br/pub/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/2/3/23510_1.png",
+    valor: "R$60,00",
+  },
+  {
+    id: 2,
+    nome: "Boneca Funko Pop: chewbacca",
+    img: "https://cf.shopee.com.br/file/07fc93bfa2b956f96a41623b00c60d3e",
+    valor: "R$60,00",
+  },
+  {
+    id: 3,
+    nome: "Almofada chewbacca",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWCS6578jrPLypvnwwLi8SZeq8IdQo6UzHcRRLyoa0O1zcuvaDFXpYnH9zgiFxcBxfKi0&usqp=CAU",
+    valor: "R$60,00",
+  },
+  {
+    id: 4,
+    nome: "Almofada R2-D2",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFO_5sGr-MviXoxPtdLr010k6fdW9SYf0qlw&usqp=CAU",
+    valor: "R$60,00",
+  },
+  {
+    id: 5,
+    nome: "Almofada de pescoço",
+    img: "http://nerdivinas.com.br/assets/images/UEVa0LIwFL5p3aDDDWEVrO9aIwZ-MxWA5yVhlzXYYxt6KMHdZEtM8.jpg",
+    valor: "R$60,00",
+  },
+  {
+    id: 6,
+    nome: "caneca StarWars",
+    img: "https://lumuspartum.com.br/wp-content/uploads/2021/05/0008_Caneca_StarWars.png",
+    valor: "R$60,00",
+  },
+];
+
+export const produtosConsole = [
+  {
+    id: 1,
+    nome: "Contole Super nintendo",
+    img: "https://www.arkade.com.br/wp-content/uploads/2018/08/Nintendo-Super-NES-Controller-700x461.jpg",
+    valor: "R$60,00",
+  },
+  {
+    id: 2,
+    nome: "PS5 ",
+    img: "https://static.netshoes.com.br/produtos/console-playstation-5-ps5-sony/14/D32-2445-014/D32-2445-014_zoom1.jpg?ts=1602777382&ims=544x",
+    valor: "R$60,00",
+  },
+  {
+    id: 3,
+    nome: "Controle leve 2 pague 1",
+    img: "https://escolabrasileiradegames.com.br/wp2016/wp-content/uploads/2020/06/escola-brasileira-de-games-9%C2%AA-gera%C3%A7%C3%A3o-de-consoles-1024x576.jpg",
+    valor: "R$60,00",
+  },
+  {
+    id: 4,
+    nome: "brink game antigo",
+    img: "https://a-static.mlcdn.com.br/800x560/console-mini-game-retro-antigo-jogo-tetris-corrida-9999-em-1-art-brink/mundopremier/7899658302375/5ed7ed5e0ad69bc89bcd361e0eda0746.jpg",
+    valor: "R$60,00",
+  },
+  {
+    id: 5,
+    nome: "PS5 personalizado",
+    img: "https://i.ebayimg.com/images/g/nBgAAOSwKP1fyoNr/s-l300.jpg",
+    valor: "R$60,00",
+  },
+  {
+    id: 6,
+    nome: "Console portatil",
+    img: "https://cf.shopee.com.br/file/f2070162e4b468833505eee637380637",
+    valor: "R$60,00",
+  },
+];
+
+export const produtosDiversos = [
+  {
+    id: 1,
+    nome: "jogo ZELDA nintendo",
+    img: "https://www.cellshop.com/9276070-thickbox_default/jogo-zelda-linksawakening-nintendo-switch.jpg",
+    valor: "R$60,00",
+  },
+  {
+    id: 2,
+    nome: "Camisa SNESS",
+    img: "https://images-americanas.b2w.io/produtos/01/00/img/2991265/0/2991265050_1GG.jpg",
+    valor: "R$60,00",
+  },
+  {
+    id: 3,
+    nome: "camisa pikachu",
+    img: "https://www.kamekame.es/30828-large_default/pokemon-camiseta-cara-pikachu-talla-l.jpg",
+    valor: "R$60,00",
+  },
+  {
+    id: 4,
+    nome: "miniatura Phoenix",
+    img: "https://images.contentstack.io/v3/assets/blt5bbf09732528de36/blt5d46ce7c4eac7ff3/6229324bf26a4b66ea4a0c6f/2022_Promos_Phoenix-Statue_Ecomm_image_1.png",
+    valor: "R$60,00",
+  },
+  {
+    id: 5,
+    nome: "pikachu pelucia",
+    img: "https://a-static.mlcdn.com.br/1500x1500/brinquedo-boneco-de-pelucia-pokemon-pikachu-18-centimetros-sunny/magazinefuturistic/7899573626099/c44be745d7c9137e9c7ef4671af344a2.jpg",
+    valor: "R$60,00",
+  },
+  {
+    id: 6,
+    nome: "Oculos realidade Virtual",
+    img: "https://www.megaeletronicos.com:4420/img/new-1-2-3-4-8-7-123487-1639836945_1639836945.jpg",
+    valor: "R$60,00",
+  },
+];
